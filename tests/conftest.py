7c7db3e23bbff29a49762570a869acc0ddeb4d12
Jordan Borean <jborean93@gmail.com>
2020-05-18 07:10:31 +1000

    Try and fix up tests on Windows now

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,7 @@ __metaclass__ = type  # noqa (fixes E402 for the imports below)
 import locale
 import os
 import pytest
+import socket
 
 from spnego._text import (
     to_bytes,
@@ -18,27 +19,44 @@ from spnego._text import (
 
 @pytest.fixture()
 def ntlm_cred(tmpdir, monkeypatch):
-    # Use unicode credentials to test out edge cases when dealing with non-ascii chars.
-    username = u'Dȫm̈Ąiᴞ\\ÜseӜ'
-    password = u'Pӓ$sw0r̈d'
-
-    tmp_creds = os.path.join(to_text(tmpdir), u'pÿspᴞӛgӫ TÈ$''.creds')
-    with open(tmp_creds, mode='wb') as fd:
-        fd.write(to_bytes(u'%s:%s:%s' % (username.split('\\')[0], username.split('\\')[1], password)))
-
-    monkeypatch.setenv('NTLM_USER_FILE', to_native(tmp_creds))
-
-    # gss-ntlmssp does a string comparison of the user/domain part using the current process locale settings. To ensure
-    # it matches the credentials we specify with the non-ascii chars we need to ensure the locale is something that can
-    # support UTF-8 character comparison. macOS can fail with unknown locale on getlocale(), just default to env vars
-    # if this get fails.
+    cleanup = None
     try:
-        original_locale = locale.getlocale(locale.LC_CTYPE)
-    except ValueError:
-        original_locale = (None, None)
+        # Use unicode credentials to test out edge cases when dealing with non-ascii chars.
+        username = 'ÜseӜ'
+        password = u'Pӓ$sw0r̈d'
 
-    locale.setlocale(locale.LC_CTYPE, 'en_US.UTF-8')
-    try:
-        yield username, password
+        if os.name == 'nt':
+            domain = to_text(socket.gethostname())
+
+            # TODO: create test user here on Windows.
+            def cleanup():
+                pass
+
+        else:
+            domain = u'Dȫm̈Ąiᴞ'
+
+            # gss-ntlmssp does a string comparison of the user/domain part using the current process locale settings.
+            # To ensure it matches the credentials we specify with the non-ascii chars we need to ensure the locale is
+            # something that can support UTF-8 character comparison. macOS can fail with unknown locale on getlocale(),
+            # just default to env vars if this get fails.
+            try:
+                original_locale = locale.getlocale(locale.LC_CTYPE)
+            except ValueError:
+                original_locale = (None, None)
+
+            def cleanup():
+                locale.setlocale(locale.LC_CTYPE, original_locale)
+
+            locale.setlocale(locale.LC_CTYPE, 'en_US.UTF-8')
+
+        tmp_creds = os.path.join(to_text(tmpdir), u'pÿspᴞӛgӫ TÈ$''.creds')
+        with open(tmp_creds, mode='wb') as fd:
+            fd.write(to_bytes(u'%s:%s:%s' % (domain, username, password)))
+
+        monkeypatch.setenv('NTLM_USER_FILE', to_native(tmp_creds))
+
+        yield u"%s\\%s" % (domain, username), password
+
     finally:
-        locale.setlocale(locale.LC_CTYPE, original_locale)
+        if cleanup:
+            cleanup()
