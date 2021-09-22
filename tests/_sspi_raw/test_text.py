7fd6fff1290e3bfd338f1ab1cc5900cb9e46c1c5
Jordan Borean <jborean93@gmail.com>
2021-09-22 13:30:02 +1000

    Skip pyx producing some faulty sorting

--- tests/_sspi_raw/test_text.py
+++ tests/_sspi_raw/test_text.py
@@ -2,10 +2,6 @@
 # (c) 2020, Jordan Borean (@jborean93) <jborean93@gmail.com>
 # MIT License (see LICENSE or https://opensource.org/licenses/MIT)
 
-from __future__ import absolute_import, division, print_function
-
-__metaclass__ = type  # noqa (fixes E402 for the imports below)
-
 import pytest
 
 from spnego._text import to_text
