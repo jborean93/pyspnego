d8085f8f923372eb80645e264f9409038baf675d
Jordan Borean <jborean93@gmail.com>
2023-08-29 06:54:11 +1000

    Added Python 3.12 win wheels and test in CI (#67)

--- build_helpers/run-ci.sh
+++ build_helpers/run-ci.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
 
+# Set by GHA setup-python
+if [[ -n "${pythonLocation}" ]]; then
+    PATH="${pythonLocation}/bin:${PATH}"
+fi
+
 source ./build_helpers/lib.sh
 lib::setup::system_requirements
 lib::setup::python_requirements
